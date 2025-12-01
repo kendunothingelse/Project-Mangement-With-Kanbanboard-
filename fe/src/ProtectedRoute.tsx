89ca0387ea06d26ae00805d517a8a7be5219b4d6
Dun2901 <lamchidung373@gmail.com>
2025-12-01 22:44:37 +0700

    Phải return 1 component, do navigate nó là function => void nên bị lỗi

--- fe/src/ProtectedRoute.tsx
+++ fe/src/ProtectedRoute.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
-import {login} from "./api/auth";
-import {Outlet, useNavigate} from "react-router-dom";
+import React, { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
 
 export default function ProtectedRoute() {
-    const navigate = useNavigate();
-    const user = null
-    return user ? <Outlet/> : navigate("/login");
+  const navigate = useNavigate();
+  const user = true;
+
+  useEffect(() => {
+    if (!user) {
+      navigate("/login", { replace: true });
+    }
+  }, [user, navigate]);
+
+  return user ? <Outlet /> : null;
 }
